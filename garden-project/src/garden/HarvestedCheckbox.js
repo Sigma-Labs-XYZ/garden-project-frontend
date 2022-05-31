75c778e198fe420f14a19a49b13d66ba3824f0ae
JTrevena <100544113+JTrevena@users.noreply.github.com>
2022-05-31 18:06:31 +0100

    Harvest check complete

---
+++ garden-project/src/garden/HarvestedCheckbox.js
@@ -0,0 +1,73 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import React from "react";
+
+export default function HarvestedCheckbox() {
+  const [show, setShow] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+
+  const placeholderPlantID = 2;
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    const plantedCheckbox = document.getElementById("inline-checkbox-1");
+    if (plantedCheckbox.checked) {
+      setShow(true);
+    }
+  };
+  const handleHarvest = async (plant_id, quantity) => {
+    await fetch("http://garden-project.sigmalabs.co.uk/harvest", {
+      method: "PUT",
+      body: { plant_id, quantity },
+    });
+    setShow(false);
+  };
+
+  return (
+    <div>
+      <Form>
+        <div className="mb-3">
+          <Form.Check
+            inline
+            label="harvested"
+            name="group2"
+            onClick={handleShow}
+            type={"checkbox"}
+            id={`inline-checkbox-2`}
+          />
+        </div>
+      </Form>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Harvest your *plant-name*</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formQuantity">
+              <Form.Label>How many *plant-name* are you harvesting?</Form.Label>
+              <Form.Control
+                type="quantity"
+                onChange={event => setQuantity(event.target.value)}
+                placeholder="*current quantity*"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="primary"
+            onClick={e => {
+              e.preventDefault();
+              handleHarvest(placeholderPlantID, quantity);
+            }}
+          >
+            Harvest!
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
