17c55c2c265a8a40e1263d23252d6035a993107f
aycin1 <aycinkaya98@gmail.com>
2022-05-31 15:33:59 +0100

    Adding header to App

--- garden-project/src/App.js
+++ garden-project/src/App.js
@@ -1,17 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "./App.scss";
 import GardenPage from "./garden/GardenPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header.js";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/garden" element={<GardenPage />} />
-      </Routes>
-    </Router>
+    <div>
+      {<Header />}
+      <Router>
+        <Routes>
+          <Route path="/garden" element={<GardenPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
