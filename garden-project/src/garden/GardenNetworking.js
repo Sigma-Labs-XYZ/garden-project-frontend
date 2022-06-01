b34347a857dac351ad7b6a73968555574b448e12
Faye <schufaye@googlemail.com>
2022-06-01 09:24:50 +0100

    edited fetch request url

--- garden-project/src/garden/GardenNetworking.js
+++ garden-project/src/garden/GardenNetworking.js
@@ -1,5 +1,7 @@
 export async function fetchGardenInfo(id) {
-  const response = await fetch(`http://localhost:8080/garden/${id}`);
+  const response = await fetch(
+    `http://garden-project.sigmalabs.co.uk/garden/${id}`
+  );
   const data = await response.json();
   return data;
 }
