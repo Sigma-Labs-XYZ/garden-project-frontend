724f0f6799b3906d4facaade958128ee570de02d
aycin1 <aycinkaya98@gmail.com>
2022-06-01 15:29:00 +0100

    Adding carousel to dashboard

---
+++ garden-project/src/dashboard/DashboardPage.js
@@ -0,0 +1,6 @@
+import CarouselForDashboard from "./Carousel.js";
+import "./dashboard.css";
+
+export default function DashboardPage() {
+  return <CarouselForDashboard id="carouselForDashboard" />;
+}
