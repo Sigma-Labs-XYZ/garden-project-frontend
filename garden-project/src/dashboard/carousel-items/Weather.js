413e22548eba55ffc28367851a4980a6548021c8
aycin1 <aycinkaya98@gmail.com>
2022-06-02 13:55:26 +0100

    added function for dates of forecast cards and possible function for changing from post/zipcode to city (need to find APIs)

--- garden-project/src/dashboard/carousel-items/Weather.js
+++ garden-project/src/dashboard/carousel-items/Weather.js
@@ -73,24 +73,24 @@ export default function Weather() {
 
   function displayWeatherData() {
     return (
-      <div className="forecastCardMother">
-        <div className="forecastCard" id="day0">
+      <div className="forecast-card-mother">
+        <div className="forecast-card" id="day0">
           <h2>Today:</h2>
           <h4>{forecastData.description}</h4>
           <h4>Temperature: {forecastData.temperature}</h4>
           <h4>Wind: {forecastData.wind}</h4>
         </div>
-        <div className="forecastCard" id="day1">
+        <div className="forecast-card" id="day1">
           <h2>Tomorrow:</h2>
           <h4>Temperature: {forecastData.forecast[0].temperature}</h4>
           <h4>Wind: {forecastData.forecast[0].wind}</h4>
         </div>
-        <div className="forecastCard" id="day2">
+        <div className="forecast-card" id="day2">
           <h2>{getTwoDaysLater()}:</h2>
           <h4>Temperature: {forecastData.forecast[1].temperature}</h4>
           <h4>Wind: {forecastData.forecast[1].wind}</h4>
         </div>
-        <div className="forecastCard" id="day3">
+        <div className="forecast-card" id="day3">
           <h2>{getThreeDaysLater()}:</h2>
           <h4>Temperature: {forecastData.forecast[2].temperature}</h4>
           <h4>Wind: {forecastData.forecast[2].wind}</h4>
@@ -100,7 +100,7 @@ export default function Weather() {
   }
 
   return (
-    <div>
+    <div className="forecast-slide">
       <h3>The forecast in {city}:</h3>
       {displayWeatherData()}
     </div>
