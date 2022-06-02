a287817fbcf69d6389e56c20ebddef4202e5ef1a
aycin1 <aycinkaya98@gmail.com>
2022-06-02 13:12:33 +0100

    Added date functionality to the last two weather cards

--- garden-project/src/dashboard/carousel-items/Weather.js
+++ garden-project/src/dashboard/carousel-items/Weather.js
@@ -2,11 +2,44 @@ import React, { useState, useEffect } from "react";
 
 export default function Weather() {
   const [forecastData, setForecastData] = useState({});
-  const [city, setCity] = useState("Amsterdam");
+  const [gardenData, setGardenData] = useState([]);
+  const [city, setCity] = useState("");
 
   useEffect(() => {
-    fetchWeatherData();
+    async function fetchData() {
+      await fetchLocationOfUser(1);
+      await fetchWeatherData();
+    }
+    fetchData();
   }, []);
+
+  //   async function convertFromPostCodesToCities(city) {
+  //     if (parseInt(city).isNaN === true) {
+  //       //check if uk postcode or if just city
+  //     } else {
+  //       if (
+  //         parseInt(city).toString().length === 5 &&
+  //         parseInt(city) >= 1 &&
+  //         parseInt(city) <= 99950
+  //       ) {
+  //         //convert from US zip code to city
+  //         //DOESNT WORK YET AS NEED AN API KEY
+  //         const response = await fetch(
+  //           `http://maps.googleapis.com/maps/api/geocode/json?address=${city}&sensor=false`
+  //         );
+  //         const data = await response.json();
+  //         setCity(data);
+  //       }
+  //     }
+  //   }
+
+  async function fetchLocationOfUser(id) {
+    const response = await fetch(
+      `http://garden-project.sigmalabs.co.uk/garden/${id}`
+    );
+    const data = await response.json();
+    await setGardenData(data);
+  }
 
   async function fetchWeatherData() {
     const response = await fetch(
@@ -14,9 +47,29 @@ export default function Weather() {
     );
     const data = await response.json();
     setForecastData(data);
+    const location = await gardenData[0].location;
+    await setCity(location);
   }
 
-  console.log(forecastData);
+  function getTwoDaysLater() {
+    const day2 = new Date(Date.now() + 172800000);
+    const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    };
+    return new Intl.DateTimeFormat("en-GB", options).format(day2);
+  }
+
+  function getThreeDaysLater() {
+    const day3 = new Date(Date.now() + 259200000);
+    const options = {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    };
+    return new Intl.DateTimeFormat("en-GB", options).format(day3);
+  }
 
   function displayWeatherData() {
     return (
@@ -33,12 +86,12 @@ export default function Weather() {
           <h4>Wind: {forecastData.forecast[0].wind}</h4>
         </div>
         <div className="forecastCard" id="day2">
-          <h2>The day after:</h2>
+          <h2>{getTwoDaysLater()}:</h2>
           <h4>Temperature: {forecastData.forecast[1].temperature}</h4>
           <h4>Wind: {forecastData.forecast[1].wind}</h4>
         </div>
         <div className="forecastCard" id="day3">
-          <h2>The day after that:</h2>
+          <h2>{getThreeDaysLater()}:</h2>
           <h4>Temperature: {forecastData.forecast[2].temperature}</h4>
           <h4>Wind: {forecastData.forecast[2].wind}</h4>
         </div>
