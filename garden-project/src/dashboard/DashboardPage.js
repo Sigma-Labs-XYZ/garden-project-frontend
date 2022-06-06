222a4ce8d254a4bd4abf8781b699aa84e6bd076a
aycin1 <aycinkaya98@gmail.com>
2022-06-06 14:16:56 +0100

    Added header

--- garden-project/src/dashboard/DashboardPage.js
+++ garden-project/src/dashboard/DashboardPage.js
@@ -1,6 +1,12 @@
 import CarouselForDashboard from "./Carousel.js";
 import "./dashboard.css";
+import Header from "./../Header.js";
 
 export default function DashboardPage() {
-  return <CarouselForDashboard id="carousel-for-dashboard" />;
+  return (
+    <div>
+      <Header />
+      <CarouselForDashboard id="carousel-for-dashboard" />;
+    </div>
+  );
 }
