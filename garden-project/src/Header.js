ab591451e539efa6c442549cf047381afc4d49c8
aycin1 <aycinkaya98@gmail.com>
2022-06-08 16:40:07 +0100

    update

--- garden-project/src/Header.js
+++ garden-project/src/Header.js
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   const [gardenNames, setGardenNames] = useState([]);
-
   const navigate = useNavigate();
 
   useEffect(() => {
