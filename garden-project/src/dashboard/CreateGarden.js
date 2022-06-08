c2d6cf339120ac87d18d029ea629d40e517aad43
Faye <schufaye@googlemail.com>
2022-06-08 16:50:38 +0100

    fix button issue

--- garden-project/src/dashboard/CreateGarden.js
+++ garden-project/src/dashboard/CreateGarden.js
@@ -5,7 +5,7 @@ import Stack from "react-bootstrap/Stack";
 export default function CreateGarden() {
   return (
     <div className="create-garden-wrapper">
-      <Stack direction="vertical" gap={3}>
+      <Stack className="create-garden-stack" direction="vertical" gap={3}>
         <h1>Create a garden to get started...</h1>
         <CreateNewGardenForm />
       </Stack>
